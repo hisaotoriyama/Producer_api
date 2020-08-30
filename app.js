8f3daccc80edb5ad37e447c0bc9a8b85fe3f6018
hisaotoriyama <hisao.toriyama@gmail.com>
2020-08-30 10:47:57 +0900

    third commit

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ let fetch = require("node-fetch")
 let path = require('path');
 app.use(express.json());
 
-app.post("/token_receiver_server", (req,res) => {
-    console.log("good")
-    console.log(req.query.login)
-    console.log(req.query.password)
+app.post("/token_receiver_server", (req,res1) => {
     var requested_query = "login=" + req.query.login +"&password=" + req.query.password
     const options = {
         method: 'POST',
@@ -17,11 +14,16 @@ app.post("/token_receiver_server", (req,res) => {
     }
     console.log(requested_query)
     fetch('https://customerservice.rusal.com/client/v1/authorization' + '?' + requested_query, options)
-        .then((res) => res.json())
-        .then((data) => console.log(data))
-        // 次からがわからないところ
-        .then((data) => {res.status(200).data})
-}
+        .then((res2) => res2.json())
+        .then((data) => {
+        console.log(data);
+        let token = data.Result
+        console.log(token)
+        return token
+        })
+        //やはり次の行を通じてtoken_receiver.jsに戻せない。
+        .then((token) => res1.status(200).token)
+    }
 );
 
 
