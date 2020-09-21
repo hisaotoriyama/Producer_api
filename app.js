9f900fccfc19376a42e7ff3941502f64eab79715
hisaotoriyama <hisao.toriyama@gmail.com>
2020-09-21 09:39:41 +0900

    20200921

--- app.js
+++ app.js
@@ -3,31 +3,59 @@ let app = express();
 let fetch = require("node-fetch")
 let path = require('path');
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.post("/token_receiver_server", (req,res1) => {
     var requested_query = "login=" + req.query.login +"&password=" + req.query.password
     const options = {
         method: 'POST',
-        // body:    JSON.stringify(body),
         body: "",
         headers: { 'Content-Type': 'application/json' },
     }
-    console.log(requested_query)
     fetch('https://customerservice.rusal.com/client/v1/authorization' + '?' + requested_query, options)
-        .then((res2) => res2.json())
-        .then((data) => {
-        console.log(data);
-        let token = data.Result
-        console.log(token)
-        return token
-        })
-        //やはり次の行を通じてtoken_receiver.jsに戻せない。
-        .then((token) => res1.status(200).token)
+    .then((res2) => res2.json())
+    .then((data) => res1.json(data.Result))
     }
 );
+
+app.post("/rwb_receiver_server", (req,res1) => {
+    const data =  {
+        "Token": req.query.token,
+        "StartDate": "2020-09-01T01:20:42.028Z",
+        "EndDate": "2020-09-20T01:20:42.028Z"
+      };
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' },
+    }
+    fetch('https://customerservice.rusal.com/client/v1/rwb', options)
+    .then((res2) => res2.json())
+    .then((data) => res1.json(data))
+    }
+);
+
+app.post("/chemcom_receiver_server", (req,res1) => {
+    const data =  {
+        "Token": req.query.token,
+        "WagonUni": [req.query.wagonuni]
+      };
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' },
+    }
+    fetch('https://customerservice.rusal.com/client/v1/chemicalcomposition', options)
+    .then((res2) => res2.json())
+    .then((data) => res1.json(data))
+    }
+);
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello World WORLDDDD");
+});
 
 
 app.use('/', express.static(path.join( __dirname, '/private')));
 
 app.listen(3009)
-
--- private/js/token_receiver.js
+++ private/js/token_receiver.js
@@ -1,16 +1,37 @@
+// Vue.component("eachwagonunit", {
+//   props: ["arg"],
+//   template: "<tr><input type=\"checkbox\" id=\"checkbox\" v-model=\"arg.check\"><td>{{each_wagonuni}}</td></tr>"  
+// })
+
 var app = new Vue({
     el: "#app",
     data: {
+      wagonuni_numbers:"",
+      data_per_HeatNumber:"",
+      chemcom:"",
+      chemcom_per_wagonunit:"",
+      rwb_info:"",
+      wagonuni:"",
       login: "",
       password: "",
-      token:""
+      token:"",
+      select_wagonuni:"",
+      alartMarkinSi:{
+        color: "red",
+        fontSize:"36px"
+      }
     },
+
+    // computed:{
+    //   wagonunicheck: function(){
+    //     if (this.wagonuni_numbers == true){
+    //       this.toggle = true 
+    //     }
+    //   }
+    // },
   
     methods: {
       createtoken: function () {
-        console.log("tokenreceiver GO!!")
-        const data = "";
-        console.log(JSON.stringify(data))
         const headers = {
           'Accept': 'application/json',
           'Content-Type': 'application/json'
@@ -18,16 +39,77 @@ var app = new Vue({
         const d = {
           headers: headers,
           method: "POST",
-          // body: JSON.stringify(data)
           body: ""
         };
         var self = this;
-        console.log(this.login)
-        console.log(this.password)
-        return fetch("/token_receiver_server?login=" + self.login + "&password=" + self.password, d)
-        // .then(e => e.json().then(j => j.result))
-        .then((e) => {
-          self.token = e 
-        })
-      }
-    }})
+        fetch("/token_receiver_server?login=" + self.login + "&password=" + self.password, d)
+        .then(res => res.json())
+        .then(data => self.receive_rwb_info_with_data(data))
+        // .then(data => self.token= data)
+        // .then(this.receive_rwb_info())
+      },
+
+      receive_rwb_info: function () {
+        const headers = {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        };
+        const d = {
+          headers: headers,
+          method: "POST",
+          body: ""
+        };
+        var self = this;
+        fetch("/rwb_receiver_server?token=" + self.token , d)
+        .then(res => res.json())
+        .then(data => self.rwb_info= data.Result)
+        .then(info => 
+          self.wagonuni_numbers = info.map((v) => {
+            return v.WagonUni
+          })
+          )
+      },
+      receive_rwb_info_with_data: function (data) {
+        const headers = {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        };
+        const d = {
+          headers: headers,
+          method: "POST",
+          body: ""
+        };
+        var self = this;
+        fetch("/rwb_receiver_server?token=" + data , d)
+        .then(res => res.json())
+        .then(data => self.rwb_info= data.Result)
+        .then(info => 
+          self.wagonuni_numbers = info.map((v) => {
+            return v.WagonUni
+          })
+          )
+      },
+      receive_chemcom_per_unit: function () {
+        const headers = {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        };
+        const d = {
+          headers: headers,
+          method: "POST",
+          body: ""
+        };
+        var self = this;        
+        fetch("/chemcom_receiver_server?token=" + self.token + "&wagonuni=" + self.wagonuni , d)
+        .then(res => res.json())
+        .then(data => self.chemcom = data.Result)
+        .then(info => {
+          self.data_per_HeatNumber = info.map((v) => {
+            return "HeatNumber:" + v.HeatNumber + "   Si:" + v.SI + "   Fe:" + v.FE;
+          })
+        }
+        )
+
+    },
+  }
+})
